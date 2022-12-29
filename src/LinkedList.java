99abba3d2e158b4b0f43c7196b1f449c8f809c7a
Elen-b <beskrovnaia.elena@yandex.ru>
2022-12-29 22:40:43 +0500

    Homework

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,5 +1,80 @@
+import java.util.Iterator;
 
-public class LinkedList<T>{
+public class LinkedList<T> implements Iterable<T>{
+    Node<T> head, tail;
+
+    public void insert(T data) {
+        Node<T> node = new Node(data);
+        if (head == null) {
+            head = node;
+            tail = head;
+        }
+        else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public void removeFirst() {
+        head = head.next;
+    }
+
+    public void removeLast() {
+        if (head == null) {
+            return;
+        }
+        Node<T> current = head;
+
+        while (current.next != tail)
+            current = current.next;
+
+        current.next = null;
+    }
+
+    public Node<T> getTail() {
+        return tail;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    //добавляет со сдвигом
+    public void insertByIndex(int index, T data) {
+        Node<T> current = head;
+        Node<T> newNode = new Node(data);
+
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        }
+        else {
+            for(int i = 1; i < index; i++) {
+                    current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> current = LinkedList.this.getHead();
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
 
     static class Node<T> {
 
@@ -10,6 +85,17 @@ public class LinkedList<T>{
         Node(T t) {
             data = t;
             next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> listString = new LinkedList<>();
+        listString.insert("Привет");
+        listString.insert("мир");
+        listString.insertByIndex(1,"целый");
+
+        for (String s : listString) {
+            System.out.print(s + ' ');
         }
     }
 
